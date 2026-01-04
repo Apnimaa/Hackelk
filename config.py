fac66e19516363a998df929593fa14ddb3da74d3
Apnimaa <patelkinjal1212@gmail.com>
2026-01-05 03:16:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config(object):
+    
+    BOT_TOKEN = "8019668327:AAFfvVgnHJq1nC38WgDh604Wk7B3Mr7x8TE"
+    
+    API_ID = 20054245
+    
+    API_HASH = "431f22f320ed5d69225d3b3fc253fc0d"
+    ADMIN_ID = [5034929962]
+    
+    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
+    DB_NAME = "MY_BOT_DB"
+    
+    TXT_LOG = -1002375772205
+    
+    AUTH_LOG = -1002375772205
+    
+    HIT_LOG = -1002375772205
+    
+    DRM_DUMP = -1002375772205
+    # Main channel ki ID
+    CHANNEL = -1002375772205
+    # Channel ka link
+    CH_URL = "https://t.me/nsmmsksmsmsmsmm"
+    
+    OWNER = "https://t.me/Nikhil_saini_khe"
+    
+    THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
+   
+    HOST = "https://api.masterapi.tech"

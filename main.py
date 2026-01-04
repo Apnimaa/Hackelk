e6da0503d442d7619aa0eca4f64728982f015b6a
Apnimaa <patelkinjal1212@gmail.com>
2026-01-05 04:48:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,5 +41,6 @@ def home():
     return "Bot is running"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+

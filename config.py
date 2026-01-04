51ff5a9b33cc3e8a0a22c4ec400cd3fd466e5081
Apnimaa <patelkinjal1212@gmail.com>
2026-01-05 03:19:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,30 +2,30 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = "8019668327:AAFfvVgnHJq1nC38WgDh604Wk7B3Mr7x8TE"
+    BOT_TOKEN = "8457218709:AAGZpkE5j2YYE0FzoIXxe7d97qSTXcWwVyY"
     
-    API_ID = 20054245
+    API_ID = 27433400
     
-    API_HASH = "431f22f320ed5d69225d3b3fc253fc0d"
-    ADMIN_ID = [5034929962]
+    API_HASH = "1a286620de5ffe0a7d9b57e604293555"
+    ADMIN_ID = [6201066540]
     
-    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://niravpatel180503_db_user:vjWNaWhRk0gMSNyQ@cluster0.26bfgmf.mongodb.net/?appName=Cluster0"
     
     DB_NAME = "MY_BOT_DB"
     
-    TXT_LOG = -1002375772205
+    TXT_LOG = -1003510568146
     
-    AUTH_LOG = -1002375772205
+    AUTH_LOG = -1003510568146
     
-    HIT_LOG = -1002375772205
+    HIT_LOG = -1003510568146
     
-    DRM_DUMP = -1002375772205
+    DRM_DUMP = -1003510568146
     # Main channel ki ID
-    CHANNEL = -1002375772205
+    CHANNEL = -1003510568146
     # Channel ka link
-    CH_URL = "https://t.me/nsmmsksmsmsmsmm"
+    CH_URL = "https://t.me/newextractorapna"
     
-    OWNER = "https://t.me/Nikhil_saini_khe"
+    OWNER = "https://t.me/Priyansh230819"
     
     THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
    
